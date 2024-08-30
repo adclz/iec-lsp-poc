71f6df613a9d3f10b3c753d34ece5fc6b6bd77d7
adclz <clauzeladrien@gmail.com>
2024-08-30 13:38:35 +0000

    feat: selectionRanges

---
+++ packages/lsp/src/capabilities/selectionRanges.ts
@@ -0,0 +1,59 @@
+import Parser, { Query, QueryCapture } from "tree-sitter";
+import { HoverParams, Hover, MarkupKind, SelectionRangeParams, SelectionRange } from "vscode-languageserver";
+import { GlobalState } from "../server";
+import { search } from "../common/intervals";
+import { asLspRange } from "../common/calc";
+
+const hoverProvider = (globalState: GlobalState): (params: SelectionRangeParams) => Promise<SelectionRange[] | null> => {
+    const {
+        documents,
+        trees,
+        symbols
+    } = globalState
+    return async (params) => {
+        const document =  documents.get(params.textDocument.uri);
+        if (!document) {
+            return [];
+        }
+		const tree = await trees.get(document.uri);
+		if (!tree) {
+			return [];
+		}
+
+		const result: SelectionRange[] = [];
+
+		for (const position of params.positions) {
+			const stack: Parser.SyntaxNode[] = [];
+			const offset = document.offsetAt(position);
+
+			let node = tree.rootNode;
+			stack.push(node);
+
+			while (true) {
+				let child = node.namedChildren.find(candidate => {
+					return candidate.startIndex <= offset && candidate.endIndex > offset;
+				});
+
+				if (child) {
+					stack.push(child);
+					node = child;
+					continue;
+				}
+				break;
+			}
+
+			let parent: SelectionRange | undefined;
+			for (let node of stack) {
+				let range = SelectionRange.create(asLspRange(node), parent);
+				parent = range;
+			}
+			if (parent) {
+				result.push(parent);
+			}
+		}
+
+		return result;
+    }
+}
+
+export default hoverProvider
