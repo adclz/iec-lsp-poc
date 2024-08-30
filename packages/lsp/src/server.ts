759904e55a7695ca809ad4ff617095513c2bc5f5
adclz <clauzeladrien@gmail.com>
2024-08-30 13:43:32 +0000

    refactor: LSP server new features

--- packages/lsp/src/server.ts
+++ packages/lsp/src/server.ts
@@ -4,88 +4,121 @@ import {
   InitializeParams,
   DidChangeConfigurationNotification,
   TextDocumentSyncKind,
-  InitializeResult
+  InitializeResult,
+  SemanticTokenTypes,
+  SemanticTokenModifiers,
+  _Connection
 } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import Parser from "tree-sitter";
-import initQueries, { Queries } from './parser/queries';
-import semanticTokensProvider, { tokenTypes } from './capabilities/semanticTokens';
 import initParser from './parser/parser';
-import { change, validateTextDocument } from './capabilities/diagnostics';
+import initQueries, { Queries } from './parser/queries';
+import semanticTokensProvider, { tokenTypes, tokenModifiers } from './capabilities/semanticTokens';
+import documentProvider from './capabilities/diagnostics';
+import completionsProvider from "./capabilities/completions"
+import symbolsProvider from "./capabilities/symbols"
+import workspaceSymbolsProvider from "./capabilities/workspaceSymbols"
+import highlightProvider from "./capabilities/highlight"
+import hoverProvider from "./capabilities/hover"
+import referencesProvider from "./capabilities/references"
+import typeDefinitionsProvider from "./capabilities/typeDefinition"
+import selectionRangeProvider from "./capabilities/selectionRanges"
+import prepareRenameProvider from "./capabilities/prepareRename"
+import renameProvider from "./capabilities/rename"
+import foldingRangeProvider from "./capabilities/foldingRange"
+import signatureProvider from "./capabilities/signature"
 
-// Create a connection for the server. The connection uses Node's IPC as a transport
+import { GlobalScope } from './symbols/scopes/global';
 
 let connection = createConnection(ProposedFeatures.all);
 
 export type GlobalState = {
+  connection: _Connection,
   documents: Map<string, TextDocument>,
   trees: Map<string, Parser.Tree>,
   parser: Parser,
   queries: Queries,
+  symbols: Map<string, GlobalScope>,
+  diagnostics: Promise<any> | null,
+  logger: _Connection['console']
 }
 
 connection.onInitialize(async (params: InitializeParams) => {
   const result: InitializeResult = {
-    multilineTokenSupport: false,
     capabilities: {
       textDocumentSync: TextDocumentSyncKind.Incremental,
       semanticTokensProvider: {
         legend: {
           tokenTypes,
-          tokenModifiers: [],
+          tokenModifiers
         },
         range: true,
         full: false
       },
+      completionProvider: {
+        triggerCharacters: [
+          "=", "."
+        ],
+      },
+      documentSymbolProvider: true,
+      documentHighlightProvider: true,
+      hoverProvider: true,
+      referencesProvider: true,
+      typeDefinitionProvider: true,
+      selectionRangeProvider: true,
+      renameProvider: {
+        prepareProvider: true
+      },
+      workspaceSymbolProvider: true,
+      foldingRangeProvider: true,
+      signatureHelpProvider: {
+        triggerCharacters: ['(', ',', ':', '=']
+      }
     }
   };
 
-  const documents = new Map<string, TextDocument>()
-  const trees = new Map<string, Parser.Tree>() 
-  const parser = initParser()
-  const queries = initQueries()
-
-  const GlobalState: GlobalState = {
-    documents,
-    trees,
-    parser,
-    queries
+  const globals: GlobalState = {
+    connection,
+    documents: new Map<string, TextDocument>(),
+    trees: new Map<string, Parser.Tree>(),
+    parser: initParser(),
+    queries: initQueries(),
+    symbols: new Map(),
+    diagnostics: null,
+    logger: connection.console
   }
 
-  connection.onDidOpenTextDocument(e => {
-    const doc = TextDocument.create(e.textDocument.uri, 'iec', e.textDocument.version, e.textDocument.text)
-    documents.set(e.textDocument.uri, doc)
-  
-    const tree = parser.parse(doc.getText())
-    trees.set(e.textDocument.uri, tree)
-  
-    connection.sendDiagnostics(validateTextDocument(GlobalState, doc, tree))
-  })
-  
-  connection.onDidCloseTextDocument(e => {
-    documents.delete(e.textDocument.uri)
-    trees.delete(e.textDocument.uri)
-  })
-  
-  connection.onDidChangeTextDocument(event => {
-    const tree = trees.get(event.textDocument.uri)!;
-    const change = event.contentChanges[0] as change
-  
-    const doc = documents.get(event.textDocument.uri)
-    const updated = TextDocument.update(doc!, event.contentChanges, event.textDocument.version)
-  
-    connection.sendDiagnostics(validateTextDocument(GlobalState, updated, tree, change))
-  });
+  const { openDocument, editDocument, closeDocument } = documentProvider(globals)
+  connection.onDidOpenTextDocument(openDocument)
+  connection.onDidChangeTextDocument(editDocument)
+  connection.onDidCloseTextDocument(closeDocument)
 
-  const semanticTokens =  semanticTokensProvider(GlobalState, parser, queries)
-  connection.onRequest("textDocument/semanticTokens/range", semanticTokens.rangeHandler)
-
+  connection.onRequest("textDocument/semanticTokens/range", async request => await waitForDiagnostics(globals, request, semanticTokensProvider(globals)))
+  connection.onRequest("textDocument/completion", async request => await waitForDiagnostics(globals, request, completionsProvider(globals)))
+  connection.onRequest("textDocument/documentSymbol", async request => await waitForDiagnostics(globals, request, symbolsProvider(globals)))
+  connection.onRequest("textDocument/documentHighlight", async request => await waitForDiagnostics(globals, request, highlightProvider(globals)))
+  connection.onRequest("textDocument/references", async request => await waitForDiagnostics(globals, request, referencesProvider(globals)))
+  connection.onRequest("textDocument/hover", async request => await waitForDiagnostics(globals, request, hoverProvider(globals)))
+  connection.onRequest("textDocument/typeDefinition", async request => await waitForDiagnostics(globals, request, typeDefinitionsProvider(globals)))
+  connection.onRequest("textDocument/selectionRange", async request => await waitForDiagnostics(globals, request, selectionRangeProvider(globals)))
+  connection.onRequest("textDocument/prepareRename", async request => await waitForDiagnostics(globals, request, prepareRenameProvider(globals)))
+  connection.onRequest("textDocument/rename", async request => await waitForDiagnostics(globals, request, renameProvider(globals)))
+  connection.onRequest("textDocument/foldingRange", async request => await waitForDiagnostics(globals, request, foldingRangeProvider(globals)))
+  connection.onRequest("textDocument/signatureHelp", async request => await waitForDiagnostics(globals, request, signatureProvider(globals)))
+  connection.onRequest("workspace/symbol", async request => await waitForDiagnostics(globals, request, workspaceSymbolsProvider(globals)))
   return result;
 });
 
 connection.onInitialized(() => {
-  console.log("IEC 61331 LSP Server initialized");
+  console.log("IEC 61131 LSP Server initialized");
   connection.client.register(DidChangeConfigurationNotification.type, undefined);
 });
 
 connection.listen();
+
+const waitForDiagnostics = async (globals: GlobalState, request: any, cb: (...args: any[]) => Promise<any>) => {
+  if (globals.diagnostics) {
+    await globals.diagnostics
+  }
+  return await cb(request)
+}
