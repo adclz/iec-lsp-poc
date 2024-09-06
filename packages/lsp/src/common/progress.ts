bc864a51a2ce8ba6433efb738c890831357b995e
adclz <clauzeladrien@gmail.com>
2024-09-06 23:28:03 +0000

    fix: remove old lsp package

--- packages/lsp/src/common/progress.ts
+++
@@ -1,7 +0,0 @@
-export const rand = function() {
-    return Math.random().toString(36).substr(2); // remove `0.`
-};
-
-export const getRandomToken = function() {
-    return rand() + rand() + rand() + "-" + rand() + rand() + rand(); // to make it longer
-};
